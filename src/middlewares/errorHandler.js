4348862d77a4e5f165b3935059ab0b66744d07b2
= <augustodemeloh@protonmail.com>
2024-03-21 22:19:21 -0300

    refactor: logError function
    new function to abstract the process of logging on error-handler middleware.

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,10 +1,7 @@
 import { ValidationError } from "sequelize";
-import { execSync } from "child_process";
-import { v4 as uuidv4 } from "uuid";
 import { createUnexpectedError, createValidationError } from "../validation/errors/controllers";
 import ApiError from "../validation/errors/classes/ApiError";
-import logger from "../logging/logger";
-import LogError from "../validation/errors/classes/LogError";
+import logError from "../logging/logError";
 
 /* eslint-disable no-unused-vars */ // error-handling middleware demands "next" to work.
 export default ({ err, source }, req, res, next) => {
@@ -18,60 +15,21 @@ export default ({ err, source }, req, res, next) => {
     const validationError = createValidationError(fields.join(", "));
     validationError.subErrors = err.errors.map((error) => error.message);
 
-    logger.error(
-      new LogError(
-        validationError.status,
-        validationError.message,
-        source,
-        {
-          node_version: process.versions.node,
-          commitHash: execSync("git rev-parse HEAD").toString().trim(),
-        },
-        uuidv4(),
-        err.stack,
-      ),
-    );
-
+    logError({ validationError, source }, err.stack);
     res.status(400).json({ error: validationError });
     return;
   }
 
   if (err instanceof ApiError) {
-    logger.error(
-      new LogError(
-        err.status,
-        err.detail,
-        source,
-        {
-          node_version: process.versions.node,
-          commitHash: execSync("git rev-parse HEAD").toString().trim(),
-        },
-        uuidv4(),
-        err.stack,
-      ),
-    );
-
     const { type, title, status, message, requestId } = err;
     const apiError = { type, title, status, message, requestId };
 
+    logError({ err, source }, err.stack);
     res.status(err.status).json({ error: apiError });
     return;
   }
 
-  logger.error(
-    new LogError(
-      err.status,
-      err.detail,
-      source,
-      {
-        node_version: process.versions.node,
-        commitHash: execSync("git rev-parse HEAD").toString().trim(),
-      },
-      uuidv4(),
-      err.stack,
-    ),
-  );
-
+  logError({ err, source }, err.stack);
   res.status(500).json({
     error: createUnexpectedError(source.path),
   });
