d519a1322c529038f235dbef135bb6f3b87469b5
= <augustodemeloh@protonmail.com>
2024-03-22 23:08:24 -0300

    refactor: uploadPhoto error handling
    instead of sending error to error-handler directly, now they're thrown and just then are captured and sent to error-handler.

--- src/middlewares/uploadPhoto.js
+++ src/middlewares/uploadPhoto.js
@@ -3,6 +3,7 @@ import multerConfig from "../config/multer";
 import * as errors from "../validation/errors/controllers";
 import ApiError from "../validation/errors/classes/ApiError";
 import stacktrace from "stack-trace";
+import ErrorContext from "../validation/errors/classes/ErrorContext";
 
 export default (req, res, next) => {
   const upload = multer(multerConfig).single("photo");
@@ -12,17 +13,20 @@ export default (req, res, next) => {
     const fullPath = req.baseUrl + req.path;
     const trace = stacktrace.parse(err);
 
-    switch (err.code) {
-      case "LIMIT_INVALID_TYPE":
-        next({ err: new ApiError(...errors.createInvalidPhotoType()), trace });
-        break;
+    try {
+      switch (err.code) {
+        case "LIMIT_INVALID_TYPE":
+          throw new ApiError(...errors.createInvalidPhotoType());
 
-      case "LIMIT_FILE_SIZE":
-        next({ err: new ApiError(...errors.createInvalidPhotoSize(fullPath)), trace });
-        break;
+        case "LIMIT_FILE_SIZE":
+          throw new ApiError(...errors.createInvalidPhotoSize(fullPath));
 
-      default:
-        next({ err: new ApiError(...errors.createUnexpectedError(fullPath)), trace });
+        default:
+          throw new ApiError(...errors.createUnexpectedError(fullPath));
+      }
+    } catch (err) {
+      const errorContext = new ErrorContext(err, trace);
+      next(errorContext);
     }
   });
 };
