a4d077d0d941f26137a2550d3e0720b4540f1cbb
= <augustodemeloh@protonmail.com>
2024-03-25 09:58:10 -0300

    refactor: fileName since the root folder
    To ensure 'trace' starts from "src" folder - not showing unnecessary parts of the path, making 'trace' a relative path.

--- src/validation/errors/getSource.js
+++ src/validation/errors/getSource.js
@@ -1,6 +1,8 @@
 export default (trace) => {
+  const regex = /src(.*)/;
+
   const functionName = trace[0].getFunctionName();
-  const fileName = trace[0].getFileName();
+  const fileName = trace[0].getFileName().match(regex)[0];
   const lineNumber = trace[0].getLineNumber();
   return { functionName, fileName, lineNumber };
 };
